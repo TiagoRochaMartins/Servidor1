89bbd50345e3e66df797e5e8c8d7d0d6a95095e5
TiagoRochaMartins <tiagorochamartins19@gmail.com>
2023-06-19 12:28:21 +0100

    mais um

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './ficheiro1.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Página não encontrada');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (filePath === './FicheirosHTMLWEB/ficheiro1.html') {
+        data = data.toString()
+          .replace('<a href="ficheiro2.html">Abrir Página 2</a>', '<a href="/ficheiro2.html">Abrir Página 2</a>')
+
+      }
+      res.end(data);
+    }
+  });
+});
+
+server.listen(8000, () => {
+  console.log('Servidor em execução na porta 8000');
+  
+});
